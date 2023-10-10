32c10921190d0770f8dd530c5dd534d00d5caeee
timeealile <timeea.lile02@e-uvt.ro>
2023-10-10 08:57:45 +0300

    initial project setup

---
+++ src/main/java/com/example/splab/SpLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.splab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpLabApplication.class, args);
+    }
+
+}
