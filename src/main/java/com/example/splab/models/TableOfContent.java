4efe1a3c3a0a3855a6db3836faf4828bd1d7695c
timeealile <timeea.lile02@e-uvt.ro>
2023-11-14 08:44:17 +0200

    lab6

---
+++ src/main/java/com/example/splab/models/TableOfContent.java
@@ -0,0 +1,23 @@
+package com.example.splab.models;
+
+import com.example.splab.models.Element;
+
+public class TableOfContent implements Element {
+    public void print(){
+        System.out.println("Sample");
+    }
+    @Override
+    public void add(Element e){
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeElement(Element e){
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Element get(int i){
+        throw new UnsupportedOperationException();
+    }
+}
